f0e47935e27182eba7e7fa212b1e82cc8311d1c7
Kanaga2 <priyakanaga112@gmail.com>
2017-07-03 13:30:15 +0530

    Create Primeno.java

---
+++ Primeno.java
@@ -0,0 +1,30 @@
+# Primeno
+import java.io.*;
+import java.util.*;
+public class Primeno
+{
+public static void main(String args[])
+{
+int n1,n2;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the starting interval");
+n1=sc.nextInt();
+System.out.println("Enter the ending interval");
+n2=sc.nextInt();
+for(int i=1;i<n;i++)
+{
+if(n%i==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+System.out.println("print prime numbers between n1&n2);
+}
+else
+{
+System.out.println("print numbers from n1 to n2");
+}
+}
+}
